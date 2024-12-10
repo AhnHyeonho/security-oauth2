70c509f79043c7396203cca0e842d8edfc826a45
안현호(Hyeonho Ahn)/Biz플랫폼개발팀/SKP <hhahn@sk.com>
2024-12-10 16:37:04 +0900

    241210-first

--- spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -1,16 +1,24 @@
 package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().authenticated();
-        http.formLogin();
-        http.apply()
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated()
+        );
+        http.formLogin(Customizer.withDefaults());
+        http.with(new CustomSecurityConfigurer().setFlag(true), Customizer.withDefaults());
+
         return http.build();
     }
 
